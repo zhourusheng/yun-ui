65f3de4122e5fefe74c086b8b6daecb39bd44e11
zhourusheng <nfuzrs@126.com>
2020-04-25 20:06:39 +0800

    基础按钮

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react'
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello world</h1>
-      <code>
-        const a = 1
-      </code>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>Hello</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+        Hello
+      </Button>
+      <Button btnType={ButtonType.Link} href='baidu.com'>
+        Hello link
+      </Button>
     </div>
   )
 }
