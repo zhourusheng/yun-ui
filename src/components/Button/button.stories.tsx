8a324b57224d7592178b868ec34a876e748156de
zhourusheng <zhourusheng@yunzhangfang.com>
2020-11-05 16:57:48 +0800

    按钮类型 type

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -2,12 +2,20 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 
-import Button from './button'
+import Button from './index'
 import '../../styles/index.scss'
 
-const defaultButton = () => (
-  <Button onClick={action('clicked')}>默认按钮</Button>
+const buttonType = () => (
+  <div
+    style={{ display: 'flex', justifyContent: 'space-around', margin: '50px' }}
+  >
+    <Button type='primary'>主按钮</Button>
+    <Button>默认按钮</Button>
+    <Button type='danger'>危险按钮</Button>
+    <Button type='link' href='/'>
+      文本按钮
+    </Button>
+  </div>
 )
 
-storiesOf('Button Component', module)
-.add('默认 Button', defaultButton)
+storiesOf('Button 按钮', module).add('按钮类型', buttonType)
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,38 +1,31 @@
-import React, { FC, ReactNode, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react'
+import React, {
+  FC,
+  ReactNode,
+  ButtonHTMLAttributes,
+  AnchorHTMLAttributes
+} from 'react'
 import classNames from 'classnames'
 
-// enum
-export enum ButtonSize {
-  Large = 'lg',
-  Small = 'sm'
-}
+export type ButtonSize = 'lg' | 'sm'
+export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
 
-export enum ButtonType {
-  Primary = 'primary',
-  Default = 'default',
-  Danger = 'danger',
-  Link = 'link'
-}
-
-// props interface
 interface BaseButtonProps {
   className?: string
   disabled?: boolean
   size?: ButtonSize
-  btnType?: ButtonType
-  children:ReactNode
+  type?: ButtonType
+  children: ReactNode
   href?: string
 }
 
-// 交叉类型 &  联合类型 |
 type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>
 type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>
-// 使用 ts Partial
+
 export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
 
-const Button:FC<ButtonProps> = props => {
+const Button: FC<ButtonProps> = props => {
   const {
-    btnType,
+    type,
     className,
     disabled,
     size,
@@ -44,15 +37,13 @@ const Button:FC<ButtonProps> = props => {
   // prefix
   const btnPrefix = 'yun-btn'
 
-  // btn btn-lg btn-primary
   const classes = classNames(btnPrefix, className, {
-    [`${btnPrefix}-${btnType}`]: btnType,
+    [`${btnPrefix}-${type}`]: type,
     [`${btnPrefix}-${size}`]: size,
-    disabled: btnType === ButtonType.Link && disabled
+    disabled: type === 'link' && disabled
   })
 
-  // link button
-  if (btnType === ButtonType.Link && href) {
+  if (type === 'link') {
     return (
       <a href={href} className={classes} {...restProps}>
         {children}
@@ -69,7 +60,7 @@ const Button:FC<ButtonProps> = props => {
 
 Button.defaultProps = {
   disabled: false,
-  btnType: ButtonType.Default
+  type: 'default'
 }
 
 export default Button
